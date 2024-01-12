337d9fd9f5ec7b29f2fa3c289b3b8fed76573792
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-12 19:57:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// Codice PHP
+    $variabile = "Giuseppe";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>
+        Ciao Amin mi chiamo  <?php echo $variabile ?>
+    </h1>
+
+
+    
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,25 @@
+<?php
+// Codice PHP
+  
+?>
+
+
+
+<form action="./script.php" method="GET">
+    <div>
+        <label for="autostrada">inserisci nome Autostrada</label>  
+    </div>
+    <div>
+        <input type="text" name="autostrada" id="autostrada">
+    </div>
+    <div>
+        <label for="autostrada">inserisci Km Autostrada</label>  
+    </div>
+    <div>
+        <input type="number" name="autostrada" id="autostrada">
+    </div>
+ 
+    <button type="submit">Invia</button>
+
+
+</form> 
